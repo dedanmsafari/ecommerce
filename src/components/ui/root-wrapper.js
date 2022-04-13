3eb1969ef6abbce4a33b12baf54029a53a5b3ab8
dedanmsafari <dedan.msafari@gmail.com>
2022-04-13 14:39:49 +0300

    deleted extra files

--- src/components/ui/root-wrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { ThemeProvider } from "@material-ui/core/styles"
-import theme from "./theme"
-
-export default ({ element }) => {
-  return <ThemeProvider theme={theme}>{element}</ThemeProvider>
-}
